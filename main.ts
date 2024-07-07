7ffde4350877e9a817bf2108097f9c5603105ba3
Sarwat Jahan <159925204+SarwatJahan22@users.noreply.github.com>
2024-07-07 16:54:17 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+// // Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+
+// // let car = 'subaru';
+
+// // console.log("Is car == 'subaru'? I predict True.")
+
+// console.log(car == 'subaru')
+
+let car = "subaru";
+
+console.log("is car == 'subaru'? predict true")
+console.log(car == 'subaru')
+
+console.log("is car != 'honda city? predict true")
+console.log(car != 'honda city') 
+
+console.log("is car == 'subaru'? predict false");
+console.log(car == 'subaru')
+
+console.log("is car == 'SUBARU'? predict false")
+console.log(car == 'SUBARU')
+
+
+console.log("is car.length == 6? predict true")
+console.log(car.length == 6)
+
+console.log("is car.length !=10? predict true")
+console.log(car.length !=10)
